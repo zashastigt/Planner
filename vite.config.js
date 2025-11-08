aa3b16288bf6353724709f8161516992963d9ce3
Rick <rickgroenevelt2000@gmail.com>
2025-11-08 22:37:48 +0100

    Add production url to allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     port: 8080,
     strictPort: true,
     host: true,
-    origin: "http:0.0.0.0:8080"
+    origin: "http://0.0.0.0:8080",
+    allowedHosts: ["planner.sibway.app"]
   }
 })
