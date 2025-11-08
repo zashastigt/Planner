2997fc75965797bb493f83f72d818f9bd9b5caa1
Rick <rickgroenevelt2000@gmail.com>
2025-11-08 22:10:52 +0100

    Add docker support

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http:0.0.0.0:8080"
+  }
 })
