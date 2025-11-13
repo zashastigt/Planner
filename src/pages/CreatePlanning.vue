e5c1fa94ef7c97024933131b21889cbe83f069ec
Zasha <zstigt@gmail.com.com>
2025-11-13 22:10:45 +0100

    add router

---
+++ src/pages/CreatePlanning.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>hi</div>
+</template>
+
+<style scoped>
+    #container {
+        display: flex;
+        align-items: flex-start;
+        width: 100vw;
+        min-height: 100vh;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
